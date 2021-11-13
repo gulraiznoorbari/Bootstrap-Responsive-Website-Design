59e278cf7225309afc67d497f638d95b4e3beaa0
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-11-13 19:03:24 +0500

    Adding Files

--- App_Landing_Page/main.js
+++ App_Landing_Page/main.js
@@ -20,4 +20,21 @@ $(document).ready(function () {
             }
         }
     });
+    /* Features Carousel */
+    $(".features-carousel").owlCarousel({
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
 });
